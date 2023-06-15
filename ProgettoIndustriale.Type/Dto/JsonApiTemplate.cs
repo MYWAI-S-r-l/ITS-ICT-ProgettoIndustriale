3b9d3fa3fed5c9e0ba902fbfc6ee21bbb885582f
DAPI11-2 <136463210+DAPI11-2@users.noreply.github.com>
2023-06-15 16:57:25 +0200

    Dev feature pi12 11 (#6)
    * Make test.vue a meteo getter for province
    
    TernaManager etc
    
    TernaManager2 13/06
    
    Script Tabella TernaToken e TernaController Setup
    
    TernaToken API, Dto, Domain, Controllers, Mapper
    
    prima pulizia del codice
    
    prima pulizia del codice
    
    ripristino ProvinceController
    
    Aggiunta script creazione database (#3)
    
    * Aggiunto script per la creazione del DB e spostato i 3 "originali" in una sottocatella
    
    * commit di prova da db master
    
    * prova dopo fetch
    
    * altra prova come le altre
    
    * ????
    
    * ultima prova lo giuro non picchiatemi
    
    * Rimozione file di testo usato per i test di push
    
    ---------
    
    Co-authored-by: Alessandro Romano <alessandro.romano0399@gmail.com>
    
    Script aggiornato dopo alcune correzioni minori (#4)
    
    Bugfix DB (#5)
    
    * Bugfix DB
    
    * Bugfix DB
    
    TernaManager2 13/06
    
    TernaToken API, Dto, Domain, Controllers, Mapper
    
    * Debug TernaToken
    
    * Porta 3306
    
    ---------
    
    Co-authored-by: Enrico Barelli <enrico.barelli@zenatek.it>
    
    ApiManager, RetrieveData and ProcessConfigFile
    
    RetrieveData fino ad estrazione Dto type da JSON config
    
    Funzioni ApiManager, Script Tabella LogsApi, Update ApiConfig
    
    ApiCallLogs Domain & Dto
    
    Config classes for ApiCalls and TernaToken, UpdateTable v0.5, pre-test
    
    If(Auth) condition, setup if(Frequency)
    
    Update file config chiamate Api
    
    Check API in base a frequenza update, pt 1 + Update Mapper per ApiCallsLogs
    
    CRUD operations for ApiCallLogs
    
    Update Jsonfile api config e relativa Dto
    
    Update Init a Set nelle Domain Classes
    
    API Manager: inserimento try/catch per Retrieve Data, commenti, renamed "template" --> "apiCall", check StatusCode 200 e aggiunta CRUD ApiCallsLogs in RetrieveData
    
    RetrieveData final update pre-Testing

---
+++ ProgettoIndustriale.Type/Dto/JsonApiTemplate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgettoIndustriale.Type.Dto;
+
+public class JsonApiTemplate
+{
+    public string apiCallName { get; set; }
+    public string tableName { get; set; }
+    public string client { get; set; }
+    public string request { get; set; }
+    public string callFrequency { get; set; }
+    public string method { get; set; }
+    public bool auth { get; set; }
+    public string authType { get; set; }
+    public List<Dictionary<string, string>> parameters { get; set; }
+    public string param_type { get; set; }
+    public List<Dictionary<string, string>> headers { get; set; }
+    public string dtoClass { get; set; }
+    public string domainClass { get; set; }
+}
