dbffcf2c18667dca2023e56aa356d6b9989dd6f1
PIBE11-2 <be.progetto.industriale.11.2@accademiadigitaleliguria.it>
2023-06-19 16:08:27 +0200

    config progetto test

--- ProgettoIndustriale.TestDB/Program.cs
+++ ProgettoIndustriale.TestDB/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using ProgettoIndustriale.Type;
+using ProgettoIndustriale.Data;
+using ProgettoIndustriale.Business;
+
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
