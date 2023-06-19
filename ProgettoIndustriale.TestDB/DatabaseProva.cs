d7eb6b4eab7e6c76653833b6d39b0d90541dc1e2
PIBE11-2 <136463857+PIBE11-2@users.noreply.github.com>
2023-06-19 17:58:54 +0200

    Creazione Dbcontext per test

---
+++ ProgettoIndustriale.TestDB/DatabaseProva.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgettoIndustriale.Type;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgettoIndustriale.TestDB
+{
+    public class DatabaseProva
+    {
+        
+
+        public static ProgettoIndustrialeContext Context()
+        {
+            var contextOptions = new DbContextOptionsBuilder<ProgettoIndustrialeContext>()
+            .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Test")
+            .Options;
+            return new ProgettoIndustrialeContext(contextOptions);
+        }
+        
+
+        
+
+    }
+}
