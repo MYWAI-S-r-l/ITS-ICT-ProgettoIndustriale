2fe688fc37e126fd9cb7d5195744b179e847e7f0
PIDB11-2 <db.progetto.industriale.11.2@accademiadigitaleliguria.it>
2023-06-22 15:30:43 +0200

    Ottimizzazione script 002
    Aggiunta degli escape character "`" e il nome del database per evitare problemi

--- ProgettoIndustriale.Data/Model/Scripts/002 - change_datatypes_to_double_from_decimal.sql
+++ ProgettoIndustriale.Data/Model/Scripts/002 - change_datatypes_to_double_from_decimal.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT('ALTER TABLE ', TABLE_NAME, ' MODIFY COLUMN ', COLUMN_NAME, ' DOUBLE;') AS query
+SELECT CONCAT('ALTER TABLE `progettoindustriale`.`', TABLE_NAME, '` MODIFY COLUMN `', COLUMN_NAME, '` DOUBLE;') AS query
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'progettoindustriale' 
   AND DATA_TYPE = 'decimal';
@@ -6,27 +6,26 @@ WHERE TABLE_SCHEMA = 'progettoindustriale'
  -- Qui sotto per comodità l'output generato dallo script soprastante, nonché lo script da usare per cambiare i datatype. 
  -- Non è obbligatorio runnare lo script in alto, salvo cambiamenti al DB che allora richiedono un copia e incolla del suo output. 
  
-ALTER TABLE commodity MODIFY COLUMN value_USD DOUBLE;
-ALTER TABLE generation MODIFY COLUMN Generation_ghw DOUBLE;
-ALTER TABLE progettoindustriale.load MODIFY COLUMN forecast_total_load_MW DOUBLE; 
--- è necessario specificare il nome del database per la tabella "load"
-ALTER TABLE price MODIFY COLUMN base_price_EURxMWh DOUBLE;
-ALTER TABLE price MODIFY COLUMN incentive_component_EURxMWh DOUBLE;
-ALTER TABLE price MODIFY COLUMN unbalance_price_EURxMWh DOUBLE;
-ALTER TABLE province MODIFY COLUMN surface DOUBLE;
-ALTER TABLE province MODIFY COLUMN altitude DOUBLE;
-ALTER TABLE province MODIFY COLUMN population_density DOUBLE;
-ALTER TABLE weather MODIFY COLUMN temperature_2m_Celsius DOUBLE;
-ALTER TABLE weather MODIFY COLUMN dewpoint_2m_Celsius DOUBLE;
-ALTER TABLE weather MODIFY COLUMN relative_humidity_2m_percent DOUBLE;
-ALTER TABLE weather MODIFY COLUMN apparent_temperature_Celsius DOUBLE;
-ALTER TABLE weather MODIFY COLUMN cloudcover_percent DOUBLE;
-ALTER TABLE weather MODIFY COLUMN windspeed_10m_km_h DOUBLE;
-ALTER TABLE weather MODIFY COLUMN windspeed_80m_km_h DOUBLE;
-ALTER TABLE weather MODIFY COLUMN surface_pressure_hPa DOUBLE;
-ALTER TABLE weather MODIFY COLUMN rain_mm DOUBLE;
-ALTER TABLE weather MODIFY COLUMN snowfall_mm DOUBLE;
-ALTER TABLE weather MODIFY COLUMN shower_mm DOUBLE;
-ALTER TABLE weather MODIFY COLUMN precipitation_mm DOUBLE;
-ALTER TABLE weather MODIFY COLUMN snow_depth_meters DOUBLE;
+ALTER TABLE `progettoindustriale`.`Commodity` MODIFY COLUMN `value_USD` DOUBLE;
+ALTER TABLE `progettoindustriale`.`Generation` MODIFY COLUMN `Generation_ghw` DOUBLE;
+ALTER TABLE `progettoindustriale`.`Load` MODIFY COLUMN `forecast_total_load_MW` DOUBLE; 
+ALTER TABLE `progettoindustriale`.`Price` MODIFY COLUMN `base_price_EURxMWh` DOUBLE;
+ALTER TABLE `progettoindustriale`.`Price` MODIFY COLUMN `incentive_component_EURxMWh` DOUBLE;
+ALTER TABLE `progettoindustriale`.`Price` MODIFY COLUMN `unbalance_price_EURxMWh` DOUBLE;
+ALTER TABLE `progettoindustriale`.`Province` MODIFY COLUMN `surface` DOUBLE;
+ALTER TABLE `progettoindustriale`.`Province` MODIFY COLUMN `altitude` DOUBLE;
+ALTER TABLE `progettoindustriale`.`Province` MODIFY COLUMN `population_density` DOUBLE;
+ALTER TABLE `progettoindustriale`.`Weather` MODIFY COLUMN `temperature_2m_Celsius` DOUBLE;
+ALTER TABLE `progettoindustriale`.`Weather` MODIFY COLUMN `dewpoint_2m_Celsius` DOUBLE;
+ALTER TABLE `progettoindustriale`.`Weather` MODIFY COLUMN `relative_humidity_2m_percent` DOUBLE;
+ALTER TABLE `progettoindustriale`.`Weather` MODIFY COLUMN `apparent_temperature_Celsius` DOUBLE;
+ALTER TABLE `progettoindustriale`.`Weather` MODIFY COLUMN `cloudcover_percent` DOUBLE;
+ALTER TABLE `progettoindustriale`.`Weather` MODIFY COLUMN `windspeed_10m_km_h` DOUBLE;
+ALTER TABLE `progettoindustriale`.`Weather` MODIFY COLUMN `windspeed_80m_km_h` DOUBLE;
+ALTER TABLE `progettoindustriale`.`Weather` MODIFY COLUMN `surface_pressure_hPa` DOUBLE;
+ALTER TABLE `progettoindustriale`.`Weather` MODIFY COLUMN `rain_mm` DOUBLE;
+ALTER TABLE `progettoindustriale`.`Weather` MODIFY COLUMN `snowfall_mm` DOUBLE;
+ALTER TABLE `progettoindustriale`.`Weather` MODIFY COLUMN `shower_mm` DOUBLE;
+ALTER TABLE `progettoindustriale`.`Weather` MODIFY COLUMN `precipitation_mm` DOUBLE;
+ALTER TABLE `progettoindustriale`.`Weather` MODIFY COLUMN `snow_depth_meters` DOUBLE;
 
