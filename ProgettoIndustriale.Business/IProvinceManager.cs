7f0ec38a0f49ee9b2e093f27d8c768a7a2cfc1d9
PIBE11-2 <be.progetto.industriale.11.2@accademiadigitaleliguria.it>
2023-06-28 09:35:43 +0200

    Revert "Creazione di tutte le interfacce"
    This reverts commit 63bc54700bc4be5ed548efb64113670d9f78fdbd.

--- ProgettoIndustriale.Business/IProvinceManager.cs
+++ ProgettoIndustriale.Business/IProvinceManager.cs
@@ -4,10 +4,21 @@ using ProgettoIndustriale.Type.Domain;
 using Dto = ProgettoIndustriale.Type.Dto;
 
 namespace ProgettoIndustriale.Business;
-
+/*
 public interface IProvinceManager
 {
-        Dto.Province? GetProvince(int id);
-   
-        List<Dto.Province> GetAllProvinces();
+    Dto.Provincia? GetProvincia(string codice);
+
+    bool DeleteProvincia(string codice);
+
+    bool DeleteProvince();
+
+    Dto.Provincia? SaveProvincia(Dto.Provincia? provinciaToDelete);
+
+    List<Dto.Provincia> SaveProvince(List<Dto.Provincia> provinceToSave);
+
+    Dto.Provincia? EditProvincia(Dto.Provincia? provinciaToEdit);
+    List<Dto.Provincia> GetAllProvince();
 }
+
+*/
