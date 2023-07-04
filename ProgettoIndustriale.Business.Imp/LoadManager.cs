4b6573e57317efaf3e83e4c8d6fb993f59a301f6
PIBE11-2 <136463857+PIBE11-2@users.noreply.github.com>
2023-07-04 14:18:43 +0200

    Commenti loadManager

--- ProgettoIndustriale.Business.Imp/LoadManager.cs
+++ ProgettoIndustriale.Business.Imp/LoadManager.cs
@@ -16,17 +16,21 @@ namespace Ansaldo.Protocollo.Business.Imp
     public class LoadManager : ILoadManager
     {
         private readonly ProgettoIndustrialeContext _context;
+
         public List<Dto.Load> getAllLoads()
         {
             List<Dom.Load> loads = _context.Load.ToList();
             return MyMapper<Dom.Load, Dto.Load>.MapList(loads);
         }
 
+        // Questo metodo restituisce una lista di oggetti Load filtrata per data di inizio e data di fine
+
         public List<Load> getloadbyDates(DateTime startDate, DateTime endDate)
         {
             throw new NotImplementedException();
         }
 
+        // Questo metodo restituisce una lista di oggetti Load filtrata per nome di macrozone, usando il metodo Include per caricare la relazione con l’entità MacroZone
         public List<Load> getLoadbyMacrozone(List<string> nameMacroZone)
         {
             List<Dom.Load> loads = _context.Load.Include(x => x.MacroZone).Where(p => nameMacroZone.Contains(p.MacroZone.Name)).ToList();
@@ -34,8 +38,10 @@ namespace Ansaldo.Protocollo.Business.Imp
 
         }
 
+        // Questo metodo restituisce una lista di oggetti Dto.Load filtrata per macrozone, data di inizio e data di fine, usando il metodo Include per caricare la relazione con l’entità Date
         public List<Dto.Load> getLoadsbyFilter(string macrozone, DateTime startDate, DateTime endDate)
         {
+           
            List<Dom.Load> loads = _context.Load.Include(x=>x.Date).Where(p=>p.Date.DateTime>startDate && p.Date.DateTime < endDate).ToList();
             return MyMapper<Dom.Load, Dto.Load>.MapList(loads);
         }
