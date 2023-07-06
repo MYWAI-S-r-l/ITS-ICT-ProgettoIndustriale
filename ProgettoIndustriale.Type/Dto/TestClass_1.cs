7f80ec8da4abb012254cb574797796ce56e7c34b
PIBE11-2 <be.progetto.industriale.11.2@accademiadigitaleliguria.it>
2023-07-06 11:52:30 +0200

    Created TestClass_1 for Sara and her personal tests

---
+++ ProgettoIndustriale.Type/Dto/TestClass_1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgettoIndustriale.Type.Dto
+{
+    public class TestClass_1
+    {
+        public TestClass_1() { }
+        public List <Province> provinceList { get; set; }
+        public List <String> categoryList { get; set; }
+        
+
+    }
+}
