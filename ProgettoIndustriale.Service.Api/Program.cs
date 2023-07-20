7ec1c8086e74775131cf0ce080ea09222230a3ce
PIDB11-2 <db.progetto.industriale.11.2@accademiadigitaleliguria.it>
2023-07-20 12:33:46 +0200

    Ora lo swagger parte anche se non siamo in debug
    Tanto non ci vede nessuno

--- ProgettoIndustriale.Service.Api/Program.cs
+++ ProgettoIndustriale.Service.Api/Program.cs
@@ -48,11 +48,11 @@ builder.Services.AddElmah();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 app.UseCors("CORSPolicy");
