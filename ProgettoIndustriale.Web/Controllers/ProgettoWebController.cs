5d9e3beff73c60b1302266479bb1e5fe3dead28d
PIBE11-2 <be.progetto.industriale.11.2@accademiadigitaleliguria.it>
2023-08-24 23:25:08 +0200

    Creazione Sito web - Controller, Vue, View, Scripts
    pagina iniziale, inserita nuova barra di navigazione, icone e grafico dimostrativo, installazione libreria boostrap e amcharts tramite NPM. E' stata modificato il layout dentro la cartella shared, per inserire la nuova barra di navigazione. Sono state inserite le icone all'interno della cartella root-assets. Infine è stata fatta la compilazione dell'intero progetto con successo, senza nessun errore.

---
+++ ProgettoIndustriale.Web/Controllers/ProgettoWebController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProgettoIndustriale.Web.Controllers
+{
+    public class ProgettoWebController: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
