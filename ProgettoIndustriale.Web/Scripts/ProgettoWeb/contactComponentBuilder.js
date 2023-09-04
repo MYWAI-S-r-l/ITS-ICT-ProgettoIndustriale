ec516fb446b0f35441ec852a67300234d989aa6a
PIBE11-2 <be.progetto.industriale.11.2@accademiadigitaleliguria.it>
2023-09-04 10:15:58 +0200

    Creazione pagina contact e creazione apicallerProgettoWeb
    creazione pagina contatti, e chiamata api progetto web.

---
+++ ProgettoIndustriale.Web/Scripts/ProgettoWeb/contactComponentBuilder.js
@@ -0,0 +1,14 @@
+﻿
+import navbarprova from "../../navbar-prova.vue";
+
+import { createPopper } from '@popperjs/core';
+import { DropdownPlugin, TablePlugin } from 'bootstrap-vue'
+import contact from "../../contact.vue";
+Vue.use(createPopper);
+Vue.use(DropdownPlugin);
+Vue.component("contact", contact);
+Vue.component("navbar-prova", navbarprova);
+
+
+
+Vue.config.devtools = true;
